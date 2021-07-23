a7e8b5c4aca0c5c2b9ab1dfe87358b3c83a183fe
Sylvanus KLA <sylvanuskla@hotmail.com>
2021-07-23 19:06:43 +0000

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, Person pers)
+        {
+            _logger = logger;
+        }
+
+        public List<Person> Persons { get; set; }
+
+        public void OnGet()
+        {
+            Persons = new List<Person>
+            {
+                new Person("Kla", "Sylvanus", 60),
+                new Person("Eté", "Benoît", 22),
+                new Person("Brindou", "Junior", 25),
+                new Person("Coulibaly", "Losséni", 24)
+            };
+        }
+    }
+}
---
+++ WebApplication1/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Person
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string nom, string prenom, int age)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Age = age;
+        }
+    }
+}
